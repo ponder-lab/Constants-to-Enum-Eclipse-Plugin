ad44b4b0e3cccec2ff8cbcd46dd7ce24a6e9e057
khatchad@cse.ohio-state.edu <khatchad@cse.ohio-state.edu@a7b9ef8a-6264-11de-b47b-210fc227597e>
2009-07-30 19:46:27 +0000

    Finished scaffolding code for Bina.

---
+++ edu.ohio_state.khatchad.refactoring/src/edu/ohio_state/khatchad/refactoring/ConvertConstantsToEnumInputPage.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package edu.ohio_state.khatchad.refactoring;
+
+import org.eclipse.ltk.ui.refactoring.UserInputWizardPage;
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * @author raffi
+ *
+ */
+public class ConvertConstantsToEnumInputPage extends UserInputWizardPage {
+
+	public ConvertConstantsToEnumInputPage(String name) {
+		super(name);
+	}
+
+	public void createControl(Composite parent) {
+	}
+
+}
